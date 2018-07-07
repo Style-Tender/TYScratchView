ec0b7da49d3ff40f135174754993137d716095c2
田宇 <785072254@qq.com>
2018-07-07 08:46:30 +0800

    Initial Commit

---
+++ 图片擦除/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  图片擦除
+//
+//  Created by 田宇 on 2018/7/7.
+//  Copyright © 2018年 田宇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 图片擦除/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  图片擦除
+//
+//  Created by 田宇 on 2018/7/7.
+//  Copyright © 2018年 田宇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
