531d53d47f7968b9dba300ae5ee364e07669d3f0
田宇 <785072254@qq.com>
2018-07-07 11:30:02 +0800

    first commit

--- 图片擦除/ViewController.m
+++ 图片擦除/ViewController.m
@@ -7,8 +7,9 @@
 //
 
 #import "ViewController.h"
+#import "TYScratchView.h"
 
-@interface ViewController ()
+@interface ViewController ()<TYScratchViewDelegate>
 
 @end
 
@@ -16,7 +17,23 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+//    TYScratchView *scratchView = [[TYScratchView alloc]initWithCoverImage:[UIImage imageNamed:@"image1.png"] andRewardImage:[UIImage imageNamed:@"style.png"]];
+    
+    TYScratchView *scratchView = [[TYScratchView alloc]init];
+    scratchView.coverImage = [UIImage imageNamed:@"image1.png"];
+    scratchView.rewardImage = [UIImage imageNamed:@"style.png"];
+    scratchView.scratchWidth = 20;
+    scratchView.isAutoCleanAll = YES;
+    
+    scratchView.delegate = self;
+    
+    scratchView.frame = CGRectMake(10, 30, 300, 150);
+    
+    [self.view addSubview:scratchView];
+}
+
+-(void)TYScratchViewIsCleanAll:(TYScratchView *)scratchView{
+    NSLog(@"Scratch End");
 }
 
 
